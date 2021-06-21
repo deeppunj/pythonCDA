0d7c9343d934cab1086ec0693c8d6642f4197493
deeppunj <deep.punj@outlook.com>
2021-06-21 13:23:36 +0200

    Update LDmodel.py

--- LDmodel.py
+++ LDmodel.py
@@ -1,3 +1,27 @@
+'''
+This program calculates the real and imaginary part of
+the refractive indices for different metals.
+I am mainly interested in Gold, Silver, and Aluminium. But you can
+extend this program to include more metals.  
+
+Input parameters:
+wavelength ==> wavelength (in nm) of the exciation wave. 
+
+materials ==> 'Au' = Gold,
+              'Ag' = Sliver,
+              'Al' = Aluminium,
+              'Cu' = Copper,
+              'Air' = Air, 
+
+model ==> Choose between different option of, 
+                        "const" = constant real refractive index,
+                        "LDM" = Lorentz-Drude model
+                        "cauchy" = Cauchy Model
+
+
+'''
+
+
 #%% importing packages   
 import numpy as np
 
@@ -10,6 +34,14 @@ class LDM():
         self.wavelength = wavelength
         self.material = material
         self.model = model
+
+        # define the constants
+        c = 2.99792458E+09 # speed of light in (meters/sec)
+        angFreq = 2*pi*c/self.wavelength; # angular frequency of the incident light (rad/sec)
+
+
+        if self.material == 'Au':
+            
 
 
 
