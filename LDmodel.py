0427110bcb00dc4812b5aac1c738c7b7dd972cd7
deeppunj <deep.punj@outlook.com>
2021-06-21 16:27:55 +0200

    Update LDmodel.py

--- LDmodel.py
+++ LDmodel.py
@@ -10,7 +10,6 @@ wavelength ==> wavelength (in nm) of the exciation wave.
 materials ==> 'Au' = Gold,
               'Ag' = Sliver,
               'Al' = Aluminium,
-              'Cu' = Copper,
               'Air' = Air, 
 
 model ==> Choose between different option of, 
@@ -22,13 +21,14 @@ model ==> Choose between different option of,
 '''
 
 
-#%% importing packages   
+#%%importing packages   
 import numpy as np
 
 
-#%% make a class to the Lorentz-Drude model 
+#%% make a class to get the Lorentz-Drude model 
 
 class LDM():
+    # LDM : Lorentz-Drude model
     def __init__(self, wavelength, material, model="LDM"):
         # get the wavelength, and the material for ex. Au, Ag or other metals
         self.wavelength = wavelength
@@ -37,11 +37,34 @@ class LDM():
 
         # define the constants
         c = 2.99792458E+09 # speed of light in (meters/sec)
-        angFreq = 2*pi*c/self.wavelength; # angular frequency of the incident light (rad/sec)
+        angFreq = 2*np.pi*c/self.wavelength; # angular frequency of the incident light (rad/sec)
+        e = 1.69e-19 # mass of electron 
+        hbar = 1.0545718e-34 # in m^2kg/sec
 
 
         if self.material == 'Au':
-            
+            #Plasma frequency 
+            # omega_p = sqrt(n*e/m*epsilon0)
+            omega_p = 9.03*e/hbar
+            #oscillation strength 
+            f = [0.760, 0.024, 0.010, 0.071, 0.601, 4.384]
+            # Damping freq of each oscillator
+            Gamma = [0.053, 0.241, 0.345, 0.870, 2.494, 2.214]
+            # resonance freq of each oscillator
+            omega = [0.000, 0.816, 4.481, 8.185, 9.083, 20.29]
+        elif self.material == 'Ag':
+            omega_p = 9.01*e/hbar
+            f = [0.845, 0.065, 0.124, 0.011, 0.840, 5.646]
+            Gamma = [0.048, 3.886, 0.452, 0.065, 0.916, 2.419]
+            omega = [0.000, 0.816, 4.481, 8.185, 9.083, 20.29]
+        elif self.material == 'Al':
+            omega_p = 14.98*e/hbar
+            f = [0.523, 0.227, 0.050, 0.166, 0.030]
+            Gamma = [0.047, 0.333, 0.312, 1.351, 3.382]
+            omega = [0.000, 0.162, 1.544, 1.808, 3.473]
+        else:
+            print("Give a valid material")
+
 
 
 
